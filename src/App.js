872592fb620969c4a10c565439160fca3f54e294
VinuUD <vinudayaratne@gmail.com>
2021-04-24 08:19:25 +0530

    Deploy React app to Github pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Just the basic React project to deploy on Github.
         </p>
         <a
           className="App-link"
